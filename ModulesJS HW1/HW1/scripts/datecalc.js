a5f25fe3247e95d386081c2920ca29e157059be6
Юрий <frunzeun@gmail.com>
2021-06-06 22:09:11 +0300

    HomeWork lesson 1 Модули

---
+++ ModulesJS HW1/HW1/scripts/datecalc.js
@@ -0,0 +1,37 @@
+import { formatError } from "./utils.js"; // 2
+
+const dateTime = luxon.DateTime;
+const dateCalcResult = document.getElementById("datecalc__result");
+
+export function handleCalcDates(event) {
+    dateCalcResult.innerHTML = "";
+    event.preventDefault();
+
+    let { firstDate, secondDate } = event.target.elements;
+    firstDate = firstDate.value, secondDate = secondDate.value;
+
+    if (firstDate && secondDate) {
+        const diff = diffDates(firstDate, secondDate); // 3
+        dateCalcResult.innerHTML = diffToHtml(diff); // 4
+    }
+    else formatError;
+};
+
+export function diffDates(firstDate, secondDate) {
+    firstDate = dateTime.fromISO(firstDate);
+    secondDate = dateTime.fromISO(secondDate);
+
+    if (firstDate > secondDate)
+        secondDate = [firstDate, firstDate = secondDate][0]; // 2
+
+    return secondDate.diff(firstDate, ['years', 'months', 'days']).toObject();
+}
+
+// 3
+export const diffToHtml = diff => `
+    <span> 
+        ${diff.years ? 'Лет: ' + diff.years : ''}
+        ${diff.months ? 'Месяцев: ' + diff.months : ''}
+        ${diff.days ? 'Дней: ' + diff.days : ''}
+    </span>
+`;
---
+++ ModulesJS HW1/HW2/scripts/scriptC.js
@@ -0,0 +1,7 @@
+function scrC() {
+    console.log('скрипт C отработал');
+}
+
+//loadScript('/HW2/scripts/asyncED.js', () => { });
+
+scrC();
